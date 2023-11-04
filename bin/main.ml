bc37bcf4c81c129652ee94034783e3bc82bf98c0
Jake Faulkner <jakefaulkn@gmail.com>
2023-11-04 18:23:54 +0100

    Add logging functions

--- bin/main.ml
+++ bin/main.ml
@@ -11,6 +11,7 @@ let track client =
     | Some message ->
       if Str.string_match join_regex message 0 then
         let client_id = (Str.matched_group 1 message, (Str.matched_group 2 message |> int_of_string) == 0) in
+        Dream.log "Adding client %s" message;
         Hashtbl.add clients client_id client;
         Lwt.return_some client_id
       else loop ()
@@ -23,10 +24,14 @@ let track client =
 let forget client_id =
   Hashtbl.remove clients client_id
 
+let int_of_bool = function
+  | true -> 1
+  | false -> 0
+
 let send client_id message =
   let (room_id, player) = client_id in
   match Hashtbl.find_opt clients (room_id, not player) with
-  | Some client -> Dream.send client message
+  | Some client -> Dream.log "Sending %s_%d %s" room_id (not player |> int_of_bool) message; Dream.send client message
   | None -> Lwt.return_unit
 
 
@@ -36,10 +41,12 @@ let handle_client client =
     let rec loop () =
       match%lwt Dream.receive client with
       | Some message ->
+        Dream.log "Received %s from %s_%d" message (fst client_id) (client_id |> snd |> int_of_bool);
         let%lwt () = send client_id message in
         loop ()
       | None ->
         forget client_id;
+        Dream.log "Removing client %s_%d" (fst client_id) (client_id |> snd |> int_of_bool);
         Dream.close_websocket client
     in
     loop ()
